43e549d2311c4f9631a9eeab98b2321d896f8d4d
Alam-Jamshed <19-41727-3@student.aiub.edu>
2022-12-10 22:06:50 +0600

    Customer Order User DTO

---
+++ BLL/DTOs/CustomerDTO.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.DTOs
+{
+    internal class CustomerDTO
+    {
+        public int Id { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Name { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Email { get; set; }
+        [Required]
+        [StringLength(14)]
+        public string Phone { get; set; }
+        [Required]
+        [StringLength(50)]
+        public string Address { get; set; }
+        [Required]
+        public string ProfilePicture { get; set; }
+        public int ShippingAreaId { get; set; }
+    }
+}
