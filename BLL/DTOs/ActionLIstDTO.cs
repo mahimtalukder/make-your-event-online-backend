af30caaee9748dbcc3278d579e13c0571fec0052
Alam-Jamshed <19-41727-3@student.aiub.edu>
2022-12-10 20:27:46 +0600

    new commit

--- BLL/DTOs/ActionLIstDTO.cs
+++ BLL/DTOs/ActionLIstDTO.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace BLL.DTOs
 {
-    public class ActionLIstDTO
+    public class ActionListDTO
     {
         public int Id { get; set; }
         public string Name { get; set; }
