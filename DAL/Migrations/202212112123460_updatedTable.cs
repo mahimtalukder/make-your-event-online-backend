97063045d6bbb59844b82e149bcd95d39e2d94c6
Alam-Jamshed <19-41727-3@student.aiub.edu>
2022-12-12 03:24:51 +0600

    Migration and Seed

---
+++ DAL/Migrations/202212112123460_updatedTable.cs
@@ -0,0 +1,18 @@
+﻿namespace DAL.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class updatedTable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.ActionLists", "Name", c => c.String(nullable: false, maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.ActionLists", "Name", c => c.String(nullable: false, maxLength: 20));
+        }
+    }
+}
