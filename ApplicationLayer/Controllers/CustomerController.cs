7afe441f9629ca7f2519e170114991f23cc85473
Alam-Jamshed <19-41727-3@student.aiub.edu>
2022-12-12 02:13:47 +0600

    customer controller

---
+++ ApplicationLayer/Controllers/CustomerController.cs
@@ -0,0 +1,96 @@
+﻿using ApplicationLayer.Auth;
+using BLL.DTOs;
+using BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace ApplicationLayer.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        [Route("api/customer/add")]
+        [HttpPost]
+        public HttpResponseMessage Add(UserCustomerDTO User)
+        {
+            try
+            {
+                User.UserType = "Customer";
+                var data = UserCustomerServices.Add(User);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+
+        [Route("api/customer/get")]
+        [HttpGet]
+        [IsLogged]
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                var data = UserCustomerServices.Get();
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        [Route("api/customer/get/{id}")]
+        [HttpGet]
+        [IsLogged]
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                var data = UserCustomerServices.Get(id);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        [Route("api/customer/update")]
+        [HttpPost]
+        [IsLogged]
+        public HttpResponseMessage Update(UserCustomerDTO User)
+        {
+            try
+            {
+                var data = UserCustomerServices.Update(User);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+
+        [Route("api/customer/delete/{id}")]
+        [HttpGet]
+        [IsLogged]
+        public HttpResponseMessage Delete(int ID)
+        {
+            try
+            {
+                var data = UserCustomerServices.Delete(ID);
+                return Request.CreateResponse(HttpStatusCode.OK, data);
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+        }
+    }
+}
