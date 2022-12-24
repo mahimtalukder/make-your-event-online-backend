38c425a73bce564a4e20473bcf0da1329ed720d2
Alam-Jamshed <19-41727-3@student.aiub.edu>
2022-12-24 14:04:52 +0600

    done

--- BLL/Services/ServiceCatalogServices.cs
+++ BLL/Services/ServiceCatalogServices.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using BLL.DTOs;
+using DAL.EF.Models;
+using DAL;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +12,67 @@ namespace BLL.Services
 {
     public class ServiceCatalogServices
     {
+        public static ServiceCatalogDTO Add(ServiceCatalogDTO action)
+        {
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<ServiceCatalogDTO, ServiceCatalog>();
+                c.CreateMap<ServiceCatalog, ServiceCatalogDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var data = mapper.Map<ServiceCatalog>(action);
+            var rt = DataAccessFactory.ServiceCatalogDataAccess().Add(data);
+            if (rt != null)
+            {
+                return mapper.Map<ServiceCatalogDTO>(rt);
+            }
+            return null;
+        }
 
+        public static List<ServiceCatalogDTO> Get()
+        {
+            var data = DataAccessFactory.ServiceCatalogDataAccess().Get();
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<ServiceCatalog, ServiceCatalogDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            return mapper.Map<List<ServiceCatalogDTO>>(data);
+        }
+        public static ServiceCatalogDTO Get(int id)
+        {
+            var data = DataAccessFactory.ServiceCatalogDataAccess().Get(id);
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<ServiceCatalog, ServiceCatalogDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            return mapper.Map<ServiceCatalogDTO>(data);
+        }
+
+
+        public static ServiceCatalogDTO Update(ServiceCatalogDTO action)
+        {
+
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<ServiceCatalogDTO, ServiceCatalog>();
+                c.CreateMap<ServiceCatalog, ServiceCatalogDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            var data = mapper.Map<ServiceCatalog>(action);
+            var rt = DataAccessFactory.ServiceCatalogDataAccess().Update(data);
+            if (rt != null)
+            {
+                return mapper.Map<ServiceCatalogDTO>(rt);
+            }
+            return null;
+        }
+
+        public static ServiceCatalogDTO Delete(int id)
+        {
+            var data = DataAccessFactory.ServiceCatalogDataAccess().Delete(id);
+            var cfg = new MapperConfiguration(c => {
+                c.CreateMap<ServiceCatalog, ServiceCatalogDTO>();
+            });
+            var mapper = new Mapper(cfg);
+            return mapper.Map<ServiceCatalogDTO>(data);
+        }
     }
 }
